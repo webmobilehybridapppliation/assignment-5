d4c14214c659b4d66eb6ad7d469c5ab05fda228b
webmobilehybridapppliation <65042398+webmobilehybridapppliation@users.noreply.github.com>
2020-06-15 09:41:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,191 @@
+//chap # 1 and task ## 1
+
+alert("THIS IS MY BEAUTIFUL AND ANOTHER BEAUTIFUL BILGATE`S WEBSITE");
+
+//chap # 1 and task ## 2
+
+alert("Error! please enter a valid password.");
+
+//chap # 1 and task ## 3
+
+alert("Welcome to JS Land... <br> Happy Coding!");
+
+
+//chap # 1 and task ## 4
+
+alert("welcome to JS Land...");
+alert("Happy Coding! Prevent this page from creating additional dialogs");
+
+
+//chap # 1 and task ## 5
+
+console.log("Hello i can run js through my web browser`s console");
+
+
+//chap # 2 and task ## 1
+var username;
+
+//chap # 2 and task ## 2
+var myName ="HASNAIN ALI";
+
+//chap # 2 and task ## 3
+var titled_message ="Hello World";
+alert(titled_message); 
+
+//chap # 2 and task ## 4
+var studentname ="Shezad Ali";
+alert(studentname);
+
+
+var studentage ="18 years old";
+alert(studentage);
+
+
+var message ="certified web and mobile hybrid application";
+alert(message);
+
+//chap # 2 and task ## 5
+var display ="lemon, chock bar,mango,pineapple";
+ 
+alert(display);
+alert(display);
+alert(display);
+alert(display);
+
+
+
+//chap # 2 and task ## 6
+var email = "hasnainsardar450009@gmail.com";
+email1 = "My email address is";
+var email = email1 + "" + email;
+alert(email);
+
+
+//chap # 2 and task ## 7
+var book = "A Smarter Way to Learn Java Script";
+var email2 = "Im trying to learn from the book";
+var email3 = email2 + "" + book;
+alert(email3);
+
+
+//chap # 2 and task ## 8
+var browser ="Yeh! I can Write HTML Content through java script";
+document.write(browser);
+
+
+//chap # 2 and task ## 9
+var browser1 ="Web Development is the Best Course";
+alert(browser1);
+var browser2 ="css is the styling sheet functionality of HTML";
+document.write(browser2);
+
+
+//chap # 3 and task ## 1
+ 
+var age ="I am 20 Years Old";
+alert(age);
+
+
+//chap # 3 and task ## 2
+var visitor;
+visitor = "I have visited this site 20 times";
+alert(visitor);
+ 
+
+//chap # 3 and task ## 3
+var birthYear ="My birth year is 2000<br> My declared variable data type is number";
+document.write(birthYear);
+
+
+//chap # 3 and task ## 4
+var visiter ="Sumeera";
+var product ="Shan packets on Daraz.pk";
+var quantity ="Did order 7";
+var full =visiter  + "" + quantity + "" + product;
+document.write(full);
+
+
+
+
+
+//chap # 5 and task ## 1
+var a =6;
+var b =5;
+var f =a+b;
+var e ="sum of 6 and 5 is";
+var g = e + "" +  f;
+document.write(g);
+
+
+//chap # 5 and task ## 2
+var a1=7;
+var b2=4;
+var f3=a1-b2;
+var e4="subtraction of 7 and 4 is";
+var g5= e4 + "" + f3;
+document.write(g5);
+var a6=7;
+var b7=4;
+var f8=a6*b7;
+var e9="Multiplication of 7 and 4 is";
+var g10= e9 + "" + f8;
+document.write(g10);
+var a11=7;
+var b12=4;
+var f13=a11/b12;
+var e14="Division of 7 and 4 is";
+var g15= e14 + "" + f13;
+document.write(g15);
+var a16=7;
+var b17=4;
+var f18=a16%b17;
+var e19="Modulus of 7 and 4 is";
+var g20= e19 + "" + f18;
+document.write(g20);
+
+//chap # 5 and task ## 3
+var y;
+document.write(    "Value after variable declaration is" + "" + y  ) ;
+y=5;
+document.write("Initial Value is:" + "" + y) ;
+y++;
+document.write( "Value after increment is:" + "" + y );
+y=y++ + 7;
+document.write( "Value after addition is:" + "" + y);
+--y;
+document.write( "Value after decrement is:" + "" + y );
+ y=12 % 3;
+
+document.write( "The remainder is:" + "" + y);
+
+
+
+//chap # 5 and task ## 4
+var pKr;
+pKr=600*5;
+document.write( "Total cost to buy 5 tickets to a movie is" + "" + pKr + "" + "PKR");
+
+
+
+//chap # 5 and task ## 5
+
+var t=prompt("Enter Your Number")
+for(var s=1;s<=10;s++) 
+{
+    document.write(t+" * "+s+" = "+t*s+"<br>");
+}
+//chap # 5 and task ## 6
+var fahrenheit, celsius;
+
+fahrenheit=prompt("please enter temperature");
+
+
+ 
+fahrenheit=(celsius) * (9/5)+32;
+
+celsius=(fahrenheit-32) * (5/9);
+document.write(fahrenheit , celsius );
+
+
+
+
